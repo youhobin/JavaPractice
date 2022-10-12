e8c3a9e9df583b58855c50b6e65c303935d5b1b2
youhobin <ghdb132@naver.com>
2022-10-12 18:45:59 +0900

    자바의 정석 연습문제 15-1

---
+++ src/Ex15_1.java
@@ -0,0 +1,31 @@
+import java.io.*;
+
+public class Ex15_1 {
+
+	public static void main(String[] args) {
+		try {
+			int lineNum = Integer.parseInt(args[0]);
+			String fileName = args[1];
+			
+			File f = new File(fileName);
+			
+			if(f.exists()&&!f.isDirectory()) {
+				BufferedReader br = new BufferedReader(new FileReader(fileName));
+				String line = "";
+				int i=1;
+				
+				while((line = br.readLine())!=null && i <= lineNum) {
+					System.out.println(i+":"+line);
+					i++;
+				}
+			} else {
+				throw new FileNotFoundException(fileName +"은/는 디렉토리이거나, 존재하지 않는 파일입니다.");
+			}
+		} catch(FileNotFoundException e) {
+			System.out.println(e.getMessage());
+		} catch(Exception e) {
+			System.out.println("USAGE: java FileHead 10 FILENAME");
+		}
+	}
+
+}
